cabc50457e7c180ae1a80c8c774fc25862aed3cf
Natalie <nataliegf2014@gmail.com>
2021-10-17 19:01:56 -0300

    rotas

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'/views'));
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get('/home', (req, res)=>{
+    res.render('home');
+})
+
+app.post('/home', (req, res)=>{
+    const {usuario} = req.body;
+    res.redirect('home');
+})
+
+app.get('/', (req, res) =>{
+    res.render('login');
+});
+
+app.get('/produtos', (req, res) =>{
+    res.render('visualizaProdutos');
+});
+
+app.get('/cadastro/produtos', (req, res) =>{
+    res.render('cadastroProdutos');
+});
+
+
+app.get('/cadastro/fornecedores', (req, res) =>{
+    res.render('cadastroFornecedores');
+});
+
+app.get('/consulta', (req, res) =>{
+    res.render('consultaProdutos');
+});
+
+app.get('*', (req, res)=>{
+    res.render('pageNotFound')
+})
+
+app.listen(80, () =>{
+    console.log('Ouvindo na porta 80')
+});
