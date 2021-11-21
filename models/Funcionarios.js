6d1eea1891f792e94fcd7fd7f141600b2d26c3cd
Natalie <nataliegf2014@gmail.com>
2021-11-21 11:40:49 -0300

    cria model

--- models/Funcionarios.js
+++ models/Funcionarios.js
@@ -1,3 +1,5 @@
+const { Usuarios } = require('./Usuarios');
+
 const Funcionarios = (sequelize, DataTypes) => {
     return sequelize.define('Funcionarios', {
         cpf: DataTypes.STRING,
@@ -7,8 +9,16 @@ const Funcionarios = (sequelize, DataTypes) => {
         rua: DataTypes.STRING,
         bairro: DataTypes.STRING,
         data_nascimento: DataTypes.DATE,
-        id_user: DataTypes.INTEGER
+        id_user: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "usuario",
+                key: "id"
+            }
+        }
     });
 }
+
+Funcionarios.hasOne(Usuarios);
 
 module.exports = Funcionarios;
